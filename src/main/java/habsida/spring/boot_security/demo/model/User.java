4401beee69fba719edb1a3a3145fab6433343caa
is2 <duishenov.islam1@gmail.com>
2024-10-12 16:09:31 +0600

    commit

--- src/main/java/habsida/spring/boot_security/demo/model/User.java
+++ src/main/java/habsida/spring/boot_security/demo/model/User.java
@@ -54,7 +54,7 @@ public class User implements UserDetails {
     @Column(name = "lastName")
     private String lastName;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
--- src/test/java/habsida/spring/boot_security/demo/SpringBootSecurityDemoApplicationTests.java
+++ src/test/java/habsida/spring/boot_security/demo/SpringBootSecurityDemoApplicationTests.java
@@ -1,13 +1,46 @@
 package habsida.spring.boot_security.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringBootSecurityDemoApplicationTests {
 
-	@Test
+    @Autowired
+    private SpringBootSecurityDemoApplication app;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Test
 	void contextLoads() {
 	}
 
+    @Test
+    public void applicationStarts() {
+        app.main(new String[]{});
+    }
+
+    
+    @Test
+    @WithMockUser(roles = {"USER", "ADMIN"})
+    void adminRoleAccess() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        assertThat(auth).isNotNull();
+        assertThat(auth.getName()).isEqualTo("admin");
+        assertThat(auth.getAuthorities()).extracting("authority").containsExactly("ROLE_USER", "ROLE_ADMIN");
+    }
+
 }
+
